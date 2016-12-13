62d0c8c5257b5593be5d5131e40cfe4175a97005
aquoss <amberquoss@gmail.com>
2016-12-12 21:16:10 -0800

    adds number guessing game routes

--- starter-code/public/app.js
+++ starter-code/public/app.js
@@ -1,4 +1,40 @@
 console.log('app.js connected');
 $(document).ready(function(){
-  console.log('DOM ready');
+
+$('#guess-number-form').on('submit', function(event){
+  $.ajax({
+    method: 'GET',
+    url: '/pick-a-number',
+    data: $('#guess-number-form').serialize(),
+    success: onSuccessGuess,
+    error: onErrorGuess
+  })
+})
+
+  $('#target-number-form').on('submit',function(event){
+      $.ajax({
+        method:'POST',
+        url: '/pick-a-number',
+        data: $('#target-number-form').serialize(),
+        success: onSuccessTarget,
+        error: onErrorTarget
+      })
+  })
+
 });
+
+function onSuccessGuess(){
+  console.log('success!');
+}
+
+function onErrorGuess(){
+  console.log('uh oh');
+}
+
+function onSuccessTarget(){
+  console.log('success!');
+}
+
+function onErrorTarget(){
+  console.log('uh oh');
+}
