27e27e86a416a76853602d591127440caedc8deb
aquoss <amberquoss@gmail.com>
2016-12-12 21:17:36 -0800

    adds number guessing game and art gallery routes

--- starter-code/server.js
+++ starter-code/server.js
@@ -22,16 +22,45 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // ROUTES
 // Root Route
-
+app.get('/', function (request, response) {
+  response.sendFile('views/index.html' , { root : __dirname});
+});
 
 // Gallery View Route
-
+app.get('/art-gallery', function (request, response) {
+  response.sendFile('views/art-gallery.html' , { root : __dirname});
+});
 
 // The Number Guessing Game
 
+var targetNum = 7;
+
+app.get('/pick-a-number',function(request,response){
+  if (request.query.number>targetNum){
+    response.send("Too High");
+  } else if (request.query.number<targetNum){
+    response.send("Too Low");
+  } else {
+    response.send("Nailed it!!!");
+  }
+})
+
+app.post('/pick-a-number',function(request,response){
+  targetNum = Number(request.body.number);
+  response.send('Number updated!');
+})
 
 // Gallery
-
+var artworks = [];
+app.get('/artworks',function(request,response){
+  var newArtwork = {
+    name: request.body.title,
+    description: request.body.description,
+    artist: request.body.artist
+  }
+  artworks.push(newArtwork);
+  response.json(artworks);
+});
 
 // SERVER START
 var port = 3000;
